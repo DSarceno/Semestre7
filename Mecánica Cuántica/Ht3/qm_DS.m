c770fd5f4ca3b126b585bdb16c9093a2f3cc2265
DSarceno <dsarceno68@gmail.com>
2022-03-07 23:11:59 -0600

    Ht3, faltan 2 ejercicios

---
+++ Mecánica Cuántica/Ht3/qm_DS.m
@@ -0,0 +1,6 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*(* :Author: Diego Sarce\[NTilde]o *)*)
+(*(* :Date: March 07, 2022 *)*)
+(*(* :Description: Package with useful routines in quantum mechanics *)*)
